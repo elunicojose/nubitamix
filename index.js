143de999dee52ff37e2511b90f287e7294d5dffa
elunicojose <elunicojose@gmail.com>
2023-12-27 17:40:49 -0300

    actualizacion user y password

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
-const express = require("express")
+const express = require("express");
 
 const app = express();
 
 const port = process.env.port || 3000;
 
 app.get("/frutas", (req, res) => {
-    async function getFrutas() {
-        // get the client
-        const mysql = require('mysql2/promise');
-        // create the connection
-        const connection = await mysql.createConnection({host:'aws.connect.psdb.cloud', user: 'd6n4q619i1xfujyx69x1', database: 'nubitamix', 
-         password: 'pscale_pw_Wcgadadc38W4rmkSHxW1HAUdhETL1ZJFyBxtDLdSpZP',
-        ssl: {
-            rejectUnauthorized: false
-        }});
-        // query database
-        const [rows, fields] = await connection.execute('SELECT * FROM `frutas`');
-        res.send(rows);
-      }
-    
-      getFrutas();  
-})
+  async function getFrutas() {
+    // get the client
+    const mysql = require("mysql2/promise");
+    // create the connection
+    const connection = await mysql.createConnection({
+      host: "aws.connect.psdb.cloud",
+      user: "4n4ir9hc80qkalkmqbxu",
+      database: "nubitamix",
+      password: "pscale_pw_KpkJ1gcLO7m9feNQq2uAnFkZwGjlXYNnM0PicpRDVb0",
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    });
+    // query database
+    const [rows, fields] = await connection.execute("SELECT * FROM `frutas`");
+    res.send(rows);
+  }
 
-   app.listen(port, () => {
-    console.log(`app running on port ${port}`)
-  })
+  getFrutas();
+});
 
-
+app.listen(port, () => {
+  console.log(`app running on port ${port}`);
+});
