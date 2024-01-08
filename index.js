f801c8b4884aa71173f6cfc5743b8fa1d54c8e88
elunicojose <elunicojose@gmail.com>
2024-01-07 22:38:30 -0300

    agrego cors access

--- index.js
+++ index.js
@@ -1,40 +1,57 @@
-require('dotenv').config();
+require("dotenv").config();
+const mysql = require("mysql2/promise");
 const express = require("express");
+var cors = require("cors");
 
 const app = express();
-const port = process.env.port || 3000;
+app.use(cors());
+const puerto = process.env.port || 3000;
+
+app.get("/", (req, res) => {
+  res.send("<h1>Home!</h1>")
+})
 
 app.get("/frutas", (req, res) => {
+  console.log("en local app get...");
   async function getFrutas() {
-    // get the client
-    const mysql = require("mysql2/promise");
-    // create the connection
+    console.log("en remote app get...");
+    // Create the connection to database
     const connection = await mysql.createConnection({
-      host: process.env.host,
+      host: process.env.host || "http://localhost",
       port: 3306,
       database: "nubitamix",
-      user: process.env.user,
-      password: process.env.password,
+      user: process.env.user || "root",
+      password: process.env.password || "rootpass",
       ssl: {
         rejectUnauthorized: false,
       },
     });
 
-    connection.connect(function (err) {
-      if (err) throw err;
-      console.log("Connected!");
-    });
-
-    // query database
-    const [rows, fields] = await connection.execute("SELECT * FROM `frutas`");
-    res.send(rows);
+    // A simple SELECT query
+    try {
+      const [result, fields] = await connection.query("SELECT * FROM `frutas`");
+      console.log("resultado: ", result); // results contains rows returned by server
+      res.set('Access-Control-Allow-Origin', '*');
+      res.json(result);
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   getFrutas();
+
+  /*
+  const result = {"data": [{"nombre":"aaax","costo":"1000","flete":"2000","total":"3000.00","id":1},{"nombre":"zzzz","costo":"5000","flete":"3000","total":"8000.00","id":2},{"nombre":"xxx","costo":"2333","flete":"4222","total":"6555.00","id":3},{"nombre":"yyy","costo":"43434","flete":"54545","total":"97979.00","id":4},{"nombre":"almendras","costo":"326","flete":"1254","total":"1580.00","id":5}]}
+  const jsonResult = JSON.stringify(result);
+  console.log('result: ', result)
+  console.log('jsonResult: ', jsonResult)
+  //res.send(jsonResult);
+  res.json(result)
+  */
 });
 
-app.listen(port, () => {
-  console.log(`app running on port ${port}`);
+app.listen(puerto, () => {
+  console.log(`app running on port ${puerto}`);
 });
 
 module.exports = app;
